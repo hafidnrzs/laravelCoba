222fcacc6f55e5a2f15e568e9b02fb0063441e08
hafidnrzs <hafidnurazis@gmail.com>
2023-06-19 00:21:52 +0700

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('container')
+<h1>Ini halaman Contact</h1>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <span>Toko Tanaman Hias ABC</span>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="/" class="nav-link active" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/produk" class="nav-link">Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/contact" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/about" class="nav-link">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
